21327c4bf71ac34640f9e1eb5521fb38b2571ecb
K. M. Zawad Monsur <zawadmonsur1@gmail.com>
2023-07-17 02:10:30 +0600

    Initialized the project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <Nav>
+            <NavLink to='/'>
+                <h1>LOGO</h1>
+            </NavLink>
+        </Nav>
+    )
+}
+
+export default Navbar
